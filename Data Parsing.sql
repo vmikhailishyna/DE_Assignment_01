e35890820d3b75f3d982ce96ad7b63ea8fc35756
vmikhailishyna <vmikhailishyna@kse.org.ua>
2026-02-16 15:45:51 +0200

    Update Data Parsing.sql

--- Data Parsing.sql
+++ Data Parsing.sql
@@ -29,13 +29,19 @@ CREATE OR REPLACE TABLE users AS
            user.followers_count as followers_count,
            user.friends_count as friends_count,
            user.listed_count as listed_count,
-           user.favorites_count as favorites_count,
-           user.created_at as created_at,
+           user.favourites_count as favorites_count,
+           CAST(strptime(user.created_at, '%a %b %d %H:%M:%S +0000 %Y') AS DATE) as created_at,
            user.statuses_count as total_tweets
     FROM raw_NintendoTweets;
+
+DESCRIBE users;
+
+SELECT * FROM users;
 
 CREATE OR REPLACE TABLE tweet_hashtags AS
        SELECT
            id,
            unnest(entities.hashtags).text as hashtag_text
        FROM raw_NintendoTweets;
+
+SELECT * FROM tweet_hashtags;
